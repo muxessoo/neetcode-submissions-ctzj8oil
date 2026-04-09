f87d58a55a58bf68f50ebb09e5ed1a0a85c72226
Mukesh Kumar Karmkar <muxessoo@gmail.com>
2026-04-09 10:55:26 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let n = nums.length,
+            res = new Array(n);
+
+        for (let i = 0; i < n; i++) {
+            let prod = 1;
+            for (let j = 0; j < n; j++) {
+                if (i !== j) {
+                    prod *= nums[j];
+                }
+            }
+            res[i] = prod;
+        }
+        return res;
+    }
+}
